4c311f48795734068f0f5f023f36259ac7e3421b
Sanchit Kumar <sanchit.dabgotra@gmail.com>
2022-04-13 01:31:25 +0530

    Updated after ACM MM

--- R.py
+++ R.py
@@ -1,3 +1,6 @@
+### THIS FILE WILL CREATE VGG16 NETWORK FOR RGB MODEL AND ALL ADD HPA MODULE (WHICH IS HANDLING THE MULTI-SCALE FUSION TECHNIQUES)
+### Packages ###
+
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
@@ -16,59 +19,61 @@ from tensorflow.keras.layers import Flatten
 # from tensorflow.keras.layers.convolutional import Conv2D
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, concatenate,MaxPool2D
 
-# SIZE_X = 224#352
-# SIZE_Y = 224 #352
 SIZE_X = 352
 SIZE_Y = 352
-input_shape=(SIZE_X, SIZE_Y, 3)
+input_shape=(SIZE_X, SIZE_Y, 3) ### IMAGE SIZE
 
-class Model_r:
+class Model_r: ## MODEL RGB
     def __init__(self):
         print("R.py called--Model created")
         self.Input_r=Input(shape=input_shape)
     def build_model(self, Input_r=None):
-        # Input_r=Input(shape=input_shape)
-        # Input_r=Input(shape=input_shape)
+        
+        ### VGG 16 NETWORK
         if Input_r==None:
             Input_r=self.Input_r
-        self.conv1_1=Conv2D(filters=64,kernel_size=(3,3),padding="same", name="conv1_1_r", activation="relu",use_bias=True)(Input_r)
-        self.conv1_2=Conv2D(filters=64,kernel_size=(3,3),padding="same",name="conv1_2_r", activation="relu",use_bias=True)(self.conv1_1)
+        self.conv1_1=Conv2D(filters=64,kernel_size=(3,3),padding="same",bias_initializer='zeros', name="conv1_1_r", activation="relu",use_bias=True)(Input_r)
+        self.conv1_2=Conv2D(filters=64,kernel_size=(3,3),padding="same",bias_initializer='zeros',name="conv1_2_r", activation="relu",use_bias=True)(self.conv1_1)
         self.pool1=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_1_r")(self.conv1_2)
-        self.conv2_1=Conv2D(filters=128, kernel_size=(3,3), padding="same", name="conv2_1_r", activation="relu",use_bias=True)(self.pool1)
-        self.conv2_2=Conv2D(filters=128, kernel_size=(3,3), padding="same",name="conv2_2_r", activation="relu",use_bias=True)(self.conv2_1)
+        self.conv2_1=Conv2D(filters=128, kernel_size=(3,3), padding="same",bias_initializer='zeros', name="conv2_1_r", activation="relu",use_bias=True)(self.pool1)
+        self.conv2_2=Conv2D(filters=128, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv2_2_r", activation="relu",use_bias=True)(self.conv2_1)
         self.pool2=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_2_r")(self.conv2_2)
-        self.conv3_1=Conv2D(filters=256, kernel_size=(3,3), padding="same",name="conv3_1_r", activation="relu",use_bias=True)(self.pool2)
-        self.conv3_2=Conv2D(filters=256, kernel_size=(3,3), padding="same", name="conv3_2_r",activation="relu",use_bias=True)(self.conv3_1)
-        self.conv3_3=Conv2D(filters=256, kernel_size=(3,3), padding="same",name="conv3_3_r", activation="relu",use_bias=True)(self.conv3_2)
+        self.conv3_1=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv3_1_r", activation="relu",use_bias=True)(self.pool2)
+        self.conv3_2=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros', name="conv3_2_r",activation="relu",use_bias=True)(self.conv3_1)
+        self.conv3_3=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv3_3_r", activation="relu",use_bias=True)(self.conv3_2)
         self.pool3=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_3_r")(self.conv3_3)
-        self.conv4_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_1_r", activation="relu",use_bias=True)(self.pool3)
-        self.conv4_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_2_r", activation="relu",use_bias=True)(self.conv4_1)
-        self.conv4_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_3_r", activation="relu",use_bias=True)(self.conv4_2)
-        self.pool4=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_4_r")(self.conv4_3)
-        self.conv5_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv5_1_r", activation="relu",use_bias=True)(self.pool4)
-        self.conv5_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv5_2_r", activation="relu",use_bias=True)(self.conv5_1)
-        self.conv5_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv5_3_r", activation="relu",use_bias=True)(self.conv5_2)
-        self.pool5=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_5_r")(self.conv5_3)
+        self.conv4_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_1_r", activation="relu",use_bias=True)(self.pool3)
+        self.conv4_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_2_r", activation="relu",use_bias=True)(self.conv4_1)
+        self.conv4_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_3_r", activation="relu",use_bias=True)(self.conv4_2)
+        self.pool4=MaxPool2D(pool_size=(2,2),strides=(1,1),name="pool_4_r",padding="same")(self.conv4_3)
+        self.conv5_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",dilation_rate=2,bias_initializer='zeros',name="conv5_1_r", activation="relu",use_bias=True)(self.pool4)
+        self.conv5_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",dilation_rate=2,bias_initializer='zeros',name="conv5_2_r", activation="relu",use_bias=True)(self.conv5_1)
+        self.conv5_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",dilation_rate=2,bias_initializer='zeros',name="conv5_3_r", activation="relu",use_bias=True)(self.conv5_2)
+        self.pool5=MaxPool2D(pool_size=(2,2),strides=(1,1),name="pool_5_r",padding="same")(self.conv5_3)
 
         # rconv2_3=Conv2D(128,kernel_size=(3,3), dilation_rate=2, padding="same")(conv2_2)
         # rpool2_4=MaxPool2D(pool_size=(2,2),strides=(2,2))(rconv2_3)
 
+        ### HPA MODULE START ###
         rconv2_3=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=1, padding="same",name="rconv2_3", activation="relu",use_bias=False)(self.conv2_2)
 
         rpool2_4=MaxPool2D(pool_size=(3,3),strides=(1,1),padding="same",name="rpool_2_4")(rconv2_3)
         rconv2_4=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=3, padding="same",name="rconv2_4", activation="relu",use_bias=False)(rpool2_4)
 
         rpool2_5=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same",name="rpool_2_5")(rconv2_4)
-        rconv2_5=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=5, padding="same",name="rconv2_5", activation="relu",use_bias=False)(rpool2_5)
+        rconv2_5=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5, padding="same",name="rconv2_5", activation="relu",use_bias=False)(rpool2_5)
 
         rpool2_6=MaxPool2D(pool_size=(7,7),strides=(1,1),padding="same",name="rpool_2_6")(rconv2_5)
-        rconv2_6=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=7, padding="same",name="rconv2_6", activation="relu",use_bias=False)(rpool2_6)
+        rconv2_6=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=7, padding="same",name="rconv2_6", activation="relu",use_bias=False)(rpool2_6)
 
-        r_concat1=concatenate([rconv2_3,rconv2_4,rconv2_5, rconv2_6], axis=3)
+        r_concat1=concatenate([self.conv2_2,rconv2_3,rconv2_4,rconv2_5, rconv2_6], axis=3)
+
+        ### HPA MODULE END ###
+
+        self.rconv2_5_=Conv2D(filters=128,kernel_size=(1,1), padding="same",name="rconv2_5_", activation="relu")(r_concat1)
 
 
-        ###########
-        self.rconv2_5_=Conv2D(filters=128,kernel_size=(3,3), padding="same",name="rconv2_5_", activation="relu",use_bias=False)(r_concat1)
+        ### HPA MODULE START ###
 
         rconv3_4=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=1, padding="same",name="rconv3_4", activation="relu",use_bias=False)(self.conv3_3)
 
@@ -76,22 +81,20 @@ class Model_r:
         rconv3_5=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=3, padding="same",name="rconv3_5", activation="relu",use_bias=False)(rpool3_5)
 
         rpool3_6=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same",name="rpool_3_6")(rconv3_5)
-        rconv3_6=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=5, padding="same",name="rconv3_6", activation="relu",use_bias=False)(rpool3_6)
+        rconv3_6=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5, padding="same",name="rconv3_6", activation="relu",use_bias=False)(rpool3_6)
 
         rpool3_7=MaxPool2D(pool_size=(7,7),strides=(1,1),padding="same",name="rpool_3_7")(rconv3_6)
-        rconv3_7=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=7, padding="same",name="rconv3_7", activation="relu",use_bias=False)(rpool3_7)
+        rconv3_7=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=7, padding="same",name="rconv3_7", activation="relu",use_bias=False)(rpool3_7)
 
-        r_concat2=concatenate([rconv3_4, rconv3_5, rconv3_6, rconv3_7])
+        r_concat2=concatenate([self.conv3_3,rconv3_4, rconv3_5, rconv3_6, rconv3_7])
 
-        ###########
-        self.rconv3_6_=Conv2D(filters=128,kernel_size=(3,3), padding="same",name="rconv3_6__r", activation="relu",use_bias=False)(r_concat2)
+        ### HPA MODULE END ###
 
-        # rconv2_3=Conv2D(128,kernel_size=(3,3), dilation_rate=2, padding="same")(conv2_2)
-        # rpool2_4=MaxPool2D(pool_size=(2,2),strides=(2,2))(rconv2_3)
 
-        # self.conv5_input_shape=(3,3,512,256)
+        self.rconv3_6_=Conv2D(filters=128,kernel_size=(1,1), padding="same",name="rconv3_6__r", activation="relu")(r_concat2)
 
-        #####
+        ### HPA MODULE START ###
+
 
         self.conv5_3_t=Conv2D(filters=128, kernel_size=(3,3), padding="same", name="conv5_3_t_r", activation="relu",use_bias=True)(self.pool5)
 
@@ -100,7 +103,7 @@ class Model_r:
 
         pool6_3=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same", name="pool_6_3_r")(conv6_2)
         conv6_3=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5, padding="same",name="conv6_3_r", activation="relu",use_bias=True)(pool6_3)
-        conv6_3_=Conv2D(filters=128, kernel_size=(3,3), padding="same",name="conv6_3__r", activation="relu",use_bias=True)(concatenate([self.conv5_3_t, conv6_3],axis=3))
+        conv6_3_=Conv2D(filters=128, kernel_size=(3,3), padding="same",name="conv6_3__r", activation="relu",use_bias=True)(conv6_3)#(concatenate([self.conv5_3_t, conv6_3],axis=3))
 
 
 
@@ -112,6 +115,6 @@ class Model_r:
 
         r_concat3=concatenate([self.conv5_3_t, conv6_2, conv6_3, conv6_4, conv6_5], axis=3)
 
-        ###########
-        self.conv6_6=Conv2D(filters=128,kernel_size=(3,3), padding="same",name="conv6_6_r", activation="relu",use_bias=True)(r_concat3)
+        ### HPA MODULE START ###
 
+        self.conv6_6=Conv2D(filters=128,kernel_size=(1,1), padding="same",name="conv6_6_r", activation="relu")(r_concat3)
--- T.py
+++ T.py
@@ -1,3 +1,5 @@
+### THIS FILE WILL CREATE VGG16 NETWORK FOR THERMAL MODEL AND ALL ADD HPA MODULE (WHICH IS HANDLING THE MULTI-SCALE FUSION TECHNIQUES)
+### Packages ###
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
@@ -16,42 +18,41 @@ from tensorflow.keras.layers import Flatten
 # from tensorflow.keras.layers.convolutional import Conv2D
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, concatenate,MaxPool2D
 
-# SIZE_X = 224 #352
-# SIZE_Y = 224 # 352
-SIZE_X = 352
+SIZE_X = 352  
 SIZE_Y = 352
-input_shape=(SIZE_X, SIZE_Y, 3)
+input_shape=(SIZE_X, SIZE_Y, 3)  ### IMAGE SIZE
 
-class Model_t:
+class Model_t:  ## MODEL THERMAL
     def __init__(self):
         print("T.py called--Model created")
         self.Input_t=Input(shape=input_shape)
     def build_model(self, Input_t=None):
-        # Input_r=Input(shape=input_shape)
-        # Input_t=Input(shape=input_shape)
+       ### VGG 16 NETWORK
         if Input_t==None:
             Input_t=self.Input_t
-        self.conv1_1=Conv2D(filters=64,kernel_size=(3,3),padding="same", name="conv1_1_t", activation="relu",use_bias=True)(Input_t)
-        self.conv1_2=Conv2D(filters=64,kernel_size=(3,3),padding="same", name="conv1_2_t", activation="relu",use_bias=True)(self.conv1_1)
+        self.conv1_1=Conv2D(filters=64,kernel_size=(3,3),padding="same",bias_initializer='zeros',name="conv1_1_t", activation="relu",use_bias=True)(Input_t)
+        self.conv1_2=Conv2D(filters=64,kernel_size=(3,3),padding="same",bias_initializer='zeros', name="conv1_2_t", activation="relu",use_bias=True)(self.conv1_1)
         self.pool1=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_1_t")(self.conv1_2)
-        self.conv2_1=Conv2D(filters=128, kernel_size=(3,3), padding="same",name="conv2_1_t", activation="relu",use_bias=True)(self.pool1)
-        self.conv2_2=Conv2D(filters=128, kernel_size=(3,3), padding="same",name="conv2_2_t", activation="relu",use_bias=True)(self.conv2_1)
+        self.conv2_1=Conv2D(filters=128, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv2_1_t", activation="relu",use_bias=True)(self.pool1)
+        self.conv2_2=Conv2D(filters=128, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv2_2_t", activation="relu",use_bias=True)(self.conv2_1)
         self.pool2=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_2_t")(self.conv2_2)
-        self.conv3_1=Conv2D(filters=256, kernel_size=(3,3), padding="same",name="conv3_1_t", activation="relu",use_bias=True)(self.pool2)
-        self.conv3_2=Conv2D(filters=256, kernel_size=(3,3), padding="same",name="conv3_2_t", activation="relu",use_bias=True)(self.conv3_1)
-        self.conv3_3=Conv2D(filters=256, kernel_size=(3,3), padding="same",name="conv3_3_t", activation="relu",use_bias=True)(self.conv3_2)
+        self.conv3_1=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv3_1_t", activation="relu",use_bias=True)(self.pool2)
+        self.conv3_2=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv3_2_t", activation="relu",use_bias=True)(self.conv3_1)
+        self.conv3_3=Conv2D(filters=256, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv3_3_t", activation="relu",use_bias=True)(self.conv3_2)
         self.pool3=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_3_t")(self.conv3_3)
-        self.conv4_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_1_t", activation="relu",use_bias=True)(self.pool3)
-        self.conv4_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_2_t", activation="relu",use_bias=True)(self.conv4_1)
-        self.conv4_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv4_3_t", activation="relu",use_bias=True)(self.conv4_2)
-        self.pool4=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_4_t")(self.conv4_3)
-        self.conv5_1=Conv2D(filters=512, kernel_size=(3,3), padding="same", name="conv5_1_t", activation="relu",use_bias=True)(self.pool4)
-        self.conv5_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv5_2_t", activation="relu",use_bias=True)(self.conv5_1)
-        self.conv5_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",name="conv5_3_t", activation="relu",use_bias=True)(self.conv5_2)
-        self.pool5=MaxPool2D(pool_size=(2,2),strides=(2,2),name="pool_5_t")(self.conv5_3)
+        self.conv4_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_1_t", activation="relu",use_bias=True)(self.pool3)
+        self.conv4_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_2_t", activation="relu",use_bias=True)(self.conv4_1)
+        self.conv4_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',name="conv4_3_t", activation="relu",use_bias=True)(self.conv4_2)
+        self.pool4=MaxPool2D(pool_size=(2,2),strides=(1,1),name="pool_4_t",padding="same")(self.conv4_3)
+        self.conv5_1=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',dilation_rate=2, name="conv5_1_t", activation="relu",use_bias=True)(self.pool4)
+        self.conv5_2=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',dilation_rate=2,name="conv5_2_t", activation="relu",use_bias=True)(self.conv5_1)
+        self.conv5_3=Conv2D(filters=512, kernel_size=(3,3), padding="same",bias_initializer='zeros',dilation_rate=2,name="conv5_3_t", activation="relu",use_bias=True)(self.conv5_2)
+        self.pool5=MaxPool2D(pool_size=(2,2),strides=(1,1),name="pool_5_t",padding="same")(self.conv5_3)
 
         # tconv2_3=Conv2D(128,kernel_size=(3,3), dilation_rate=2, padding="same")(conv2_2)
         # tpool2_4=MaxPool2D(pool_size=(2,2),strides=(2,2))(tconv2_3)
+
+        ### HPA MODULE START ###
 
         tconv2_3=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=1, name="tconv2_3", padding="same", activation="relu",use_bias=True)(self.conv2_2)
 
@@ -59,16 +60,21 @@ class Model_t:
         tconv2_4=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=3,name="tconv2_4", padding="same", activation="relu",use_bias=True)(tpool2_4)
 
         tpool2_5=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same",name="tpool_2_5")(tconv2_4)
-        tconv2_5=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=5,name="tconv2_5", padding="same", activation="relu",use_bias=True)(tpool2_5)
+        tconv2_5=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5,name="tconv2_5", padding="same", activation="relu",use_bias=True)(tpool2_5)
 
         tpool2_6=MaxPool2D(pool_size=(7,7),strides=(1,1),padding="same",name="tpool_2_6")(tconv2_5)
-        tconv2_6=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=7, name="tconv2_6", padding="same", activation="relu",use_bias=True)(tpool2_6)
+        tconv2_6=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=7, name="tconv2_6", padding="same", activation="relu",use_bias=True)(tpool2_6)
 
-        t_concat1=concatenate([tconv2_3,tconv2_4,tconv2_5, tconv2_6], axis=3)
+        t_concat1=concatenate([self.conv2_2,tconv2_3,tconv2_4,tconv2_5, tconv2_6], axis=3)
+        
+        ### HPA MODULE END ###
 
 
         ###########
-        self.tconv2_5_=Conv2D(filters=128,kernel_size=(3,3), padding="same",name="tconv2_5_", activation="relu",use_bias=True)(t_concat1)
+        self.tconv2_5_=Conv2D(filters=128,kernel_size=(1,1), padding="same",name="tconv2_5_", activation="relu")(t_concat1)
+
+        ### HPA MODULE START ###
+
 
         tconv3_4=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=1, padding="same",name="tconv3_4", activation="relu",use_bias=True)(self.conv3_3)
 
@@ -76,22 +82,20 @@ class Model_t:
         tconv3_5=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=3, padding="same",name="tconv3_5", activation="relu",use_bias=True)(tpool3_5)
 
         tpool3_6=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same",name="tpool_3_6")(tconv3_5)
-        tconv3_6=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=5, padding="same",name="tconv3_6", activation="relu",use_bias=True)(tpool3_6)
+        tconv3_6=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5, padding="same",name="tconv3_6", activation="relu",use_bias=True)(tpool3_6)
 
         tpool3_7=MaxPool2D(pool_size=(7,7),strides=(1,1),padding="same",name="tpool_3_7")(tconv3_6)
-        tconv3_7=Conv2D(filters=64, kernel_size=(3,3), dilation_rate=7, padding="same",name="tconv3_7", activation="relu",use_bias=True)(tpool3_7)
+        tconv3_7=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=7, padding="same",name="tconv3_7", activation="relu",use_bias=True)(tpool3_7)
 
-        t_concat2=concatenate([tconv3_4, tconv3_5, tconv3_6, tconv3_7])
+        t_concat2=concatenate([self.conv3_3,tconv3_4, tconv3_5, tconv3_6, tconv3_7])
 
-        ###########
-        self.tconv3_6_=Conv2D(filters=128,kernel_size=(3,3), padding="same", name="tconv3_6_", activation="relu",use_bias=False)(t_concat2)
+        ### HPA MODULE END ###
 
-        # tconv2_3=Conv2D(128,kernel_size=(3,3), dilation_rate=2, padding="same")(conv2_2)
-        # tpool2_4=MaxPool2D(pool_size=(2,2),strides=(2,2))(tconv2_3)
 
-        # self.conv5_input_shape=(3,3,512,256)
+        self.tconv3_6_=Conv2D(filters=128,kernel_size=(1,1), padding="same", name="tconv3_6_", activation="relu")(t_concat2)
 
-        #####
+        ### HPA MODULE START ###
+
 
         self.conv5_3_t=Conv2D(filters=128, kernel_size=(3,3), padding="same", name="conv5_3_t_t", activation="relu",use_bias=False)(self.pool5)
 
@@ -100,7 +104,7 @@ class Model_t:
 
         pool6_3=MaxPool2D(pool_size=(5,5),strides=(1,1),padding="same",name="pool_6_3_t")(conv6_2)
         conv6_3=Conv2D(filters=128, kernel_size=(3,3), dilation_rate=5, padding="same", name="conv6_3_t", activation="relu",use_bias=False)(pool6_3)
-        conv6_3_=Conv2D(filters=128, kernel_size=(3,3), padding="same", name="conv6_3__t", activation="relu",use_bias=False)(concatenate([self.conv5_3_t, conv6_3],axis=3))
+        conv6_3_=Conv2D(filters=128, kernel_size=(3,3), padding="same", name="conv6_3__t", activation="relu",use_bias=False)(conv6_3)#(concatenate([self.conv5_3_t, conv6_3],axis=3))
 
 
 
@@ -112,6 +116,6 @@ class Model_t:
 
         t_concat3=concatenate([self.conv5_3_t, conv6_2, conv6_3, conv6_4, conv6_5], axis=3)
 
-        ###########
-        self.conv6_6=Conv2D(filters=128,kernel_size=(3,3), padding="same", name="conv6_6_t", activation="relu",use_bias=False)(t_concat3)
+        ### HPA MODULE START ###
 
+        self.conv6_6=Conv2D(filters=128,kernel_size=(1,1), padding="same", name="conv6_6_t", activation="relu")(t_concat3)
